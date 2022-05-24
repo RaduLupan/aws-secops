91998e394b0481c80183d4f27ccb50aee9317bfb
RaduLupan <github@lupan.ca>
2022-05-24 07:28:47 -0400

    Initial commit

---
+++ get_public_s3_buckets.py
@@ -0,0 +1,17 @@
+'''
+---------------------------------------------------------------------------------------------
+Description: This script loops through all the S3 buckets in an AWS account and determines if 
+public access is granted by either ACL or bucket policy.
+The public buckets are listed in a Google Sheet.
+Parameters: 
+----------------------------------------------------------------------------------------------
+'''
+
+import gsheets_api
+import aws_secops
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
