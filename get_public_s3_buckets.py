d81e079b9f8fc8c5b46d485a2170c05634991620
RaduLupan <github@lupan.ca>
2022-05-24 07:46:32 -0400

    Added the local variables for google sheets.

--- get_public_s3_buckets.py
+++ get_public_s3_buckets.py
@@ -11,7 +11,16 @@ import gsheets_api
 import aws_secops
 
 def main():
-    pass
+    
+    # Local values required for accessing Google Sheets API as a service account. Could be read from SSM Parameter Store instead.
+    scopes = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive']
+    service_account_file = 'credentials.json'
+    sample_spreadsheet_id = '1z36C1xvQwrvrxyLlYIHx5wTZFt_BpOYi-q6DF_2B39g'
+    
+    bucket_properties = dict()
+    public_buckets = []
+
+
 
 if __name__ == '__main__':
     main()
