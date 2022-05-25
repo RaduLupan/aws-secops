68db7cba62639048fc48b1f2864f01eca2365559
RaduLupan <github@lupan.ca>
2022-05-25 08:28:43 -0400

    Initial commit

---
+++ get_bucket_acl.py
@@ -0,0 +1,16 @@
+'''
+Example on how to get the ACL of a bucket.
+'''
+import boto3
+
+s3 = boto3.client('s3')
+
+bucket_name = "test"
+
+bucket_acl = s3.get_bucket_acl(Bucket=bucket_name)
+
+print(bucket_acl)
+
+grants = bucket_acl['Grants']
+
+print(f"Bucket Grants:\n {grants}")
