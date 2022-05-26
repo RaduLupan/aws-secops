55472e20312ca5febb685ce06164740d39c24dac
RaduLupan <github@lupan.ca>
2022-05-26 08:40:26 -0400

    Introduced creation of GSheet, added headers and appended serialized data using json module.

--- get_public_s3_buckets.py
+++ get_public_s3_buckets.py
@@ -11,6 +11,8 @@ import gsheets_api
 import aws_secops
 import boto3
 
+import json
+
 def main():
     
     # Local values required for accessing Google Sheets API as a service account. Could be read from SSM Parameter Store instead.
@@ -19,7 +21,13 @@ def main():
     sample_spreadsheet_id = '1z36C1xvQwrvrxyLlYIHx5wTZFt_BpOYi-q6DF_2B39g'
     
     bucket_properties = dict()
+    
+    # List of dictionaries.
     public_buckets = []
+
+    # List of rows to append to Google Sheet.
+    public_buckets_report =[]
+    row = []
 
     # Get the service client.
     s3 = boto3.client('s3')
@@ -34,6 +42,32 @@ def main():
         print("No public buckets detected. That's actually great!")
     else:
         print(f'The list of public buckets is below:\n {public_buckets}')
+
+        for bucket in public_buckets:
+            
+            row.append(bucket['Name'])
+            row.append(json.dumps(bucket['Grants'][0]))
+            row.append(json.dumps(bucket['Owner']))
+
+        # Credentials for Google Sheets API.
+        creds = gsheets_api.ServiceAccountCredentials.from_json_keyfile_name(
+        service_account_file,
+        scopes,
+        )
+
+        # Create new sheet in the existing spreadsheet.
+        gsheets_api.create_sheet(creds=creds, title='S3 Public Buckets', spreadsheet_id=sample_spreadsheet_id)
+
+        # Update newly created sheet with data from a list.
+        headers=['Name', 'Grants', 'Owner', 'PolicyStatus', 'PublicACL']
+        gsheets_api.update_sheet(creds, title = 'S3 Public Buckets', spreadsheet_id=sample_spreadsheet_id, data=headers)
+
+        # Append values to newly created sheet.
+        result = gsheets_api.append_values(creds, sample_spreadsheet_id, "S3 Public Buckets!A2:C3", "USER_ENTERED",
+             [
+                 row
+            ])
+        print(result)
 
 if __name__ == '__main__':
     main()
